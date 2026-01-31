11a0c2d82953ce0e3683ea061600061d270c1095
Nico3OGS <107954982+Nico3OGS@users.noreply.github.com>
2026-01-31 19:47:19 -0300

    Added state when there is no mask

--- hands/custom_funcion_pickup.gd
+++ hands/custom_funcion_pickup.gd
@@ -8,5 +8,6 @@ func _on_grip_pressed() -> void:
 		Global.current_mask.visible = true
 		_pick_up_object(Global.current_mask)
 		Global.current_mask = null
+		Global.change_mask.emit("none")
 	else:
 		super._on_grip_pressed()
