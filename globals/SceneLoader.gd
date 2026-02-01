7ca32bba437900bfec507094107b75aa67d54c41
Exequiel <exe4que@gmail.com>
2026-02-01 10:09:59 -0300

    Intro Transition

---
+++ globals/SceneLoader.gd
@@ -0,0 +1,13 @@
+extends Node
+
+signal scene_loaded
+signal scene_unloaded
+
+func load_scene(scene_name: String):
+	var target_scene = load(str("res://", scene_name, ".tscn")).instantiate()
+	get_tree().root.add_child(target_scene)
+	scene_loaded.emit()
+
+func unload_scene(scene_instance: Node):
+	scene_instance.queue_free()
+	scene_unloaded.emit()
